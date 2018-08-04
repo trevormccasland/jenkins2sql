702fa94daa96fefc201d67c15fc3f8d52ae6c85a
Trevor McCasland <tm2086@att.com>
2018-08-04 11:30:19 -0500

    Add artifacts linke and refactor a bit

--- jenkins2sql/api.py
+++ jenkins2sql/api.py
@@ -16,16 +16,23 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 def get_app():
     return app
 
-
-def get_metadata(json_data):
+def get_url(json_data):
     data = json.loads(json_data.text)
-    # make configurable
-    url = data['url']
+    return data['url']
+
+def set_artifacts_link(url):
+    # make build name/id configurable
+    artifact_link = url + 'artifact/results.html'
+    shell.CONF.set_override('artifacts', artifact_link)
+
+
+def set_metadata(url):
     url_parts = url.split('/')
-    return {'build_name':  url_parts[-3:-2],
-            'artifacts': url,
-            'host': url_parts[-5:-4],
-            'build_id':  '-'.join(url_parts[-3:-1])}
+    # make build name/id configurable
+    metadata = {'build_name':  url_parts[-3:-2],
+                'host': url_parts[-5:-4],
+                'build_id':  '-'.join(url_parts[-3:-1])}
+    shell.CONF.set_override('run_meta', metadata)
 
 def requests_get(url, auth=None):
     try:
@@ -34,7 +41,7 @@ def requests_get(url, auth=None):
     except requests.exceptions.RequestException as err:
         raise ValueError('Bad url: %s requests says %s' % (url, err))
     return resp
-    
+
 
 @app.route('/runs', methods=['GET', 'POST'])
 def create_run():
@@ -49,25 +56,26 @@ def create_run():
     # make configurable
     try:
         json_resp = requests_get('%s/api/json' % url, auth=(user, password))
-        subunit_data = requests_get('%s/artifact/subunit.stream' % url, 
+        subunit_data = requests_get('%s/artifact/subunit.stream' % url,
                                     auth=(user, password))
     except ValueError as err:
         return 'Failed getting build info... %s' % err
     try:
-        metadata = get_metadata(json_resp)
+        url = get_url(json_resp)
     except ValueError as err:
         return 'Looks like your test json data is bad: %s' % err
     try:
         subunit_file = io.BytesIO(subunit_data.content)
     except ValueError as err:
         return 'Looks like your subunit stream is bad: %s' % err
+
+    set_artifacts_link(url)
+    set_metadata(url)
     stream = read_subunit.ReadSubunit(subunit_file)
-    shell.process_results(stream.get_results())
-    shell.CONF.set_override('run_meta', metadata)
     shell.process_results(stream.get_results())
     return jsonify(metadata)
 
-    
+
 def main():
     config = ConfigParser.ConfigParser()
     config.read(['etc/jenkins2sql.conf'])
@@ -81,7 +89,7 @@ def main():
     shell.parse_args([])
     shell.CONF.set_override('connection', db_uri, group='database')
     app.run(debug=True, host=host, port=port)
- 
+
 
 if __name__ == "__main__":
    main()
